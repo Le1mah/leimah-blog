1278a5aadef270b4562511023a0820646af8adba
Le1mah <leimahhamiel@gmail.com>
2025-02-01 01:40:44 +0800

    feat: switch to fast-xml-parser

--- src/routes/api/bisu/makeAtom.tsx
+++ src/routes/api/bisu/makeAtom.tsx
@@ -1,25 +1,33 @@
-import { create } from 'xmlbuilder2';
+import { XMLBuilder } from 'fast-xml-parser'; // pnpm add fast-xml-parser
 import type { Article } from './parseInput';
-import { escape } from 'html-escaper';
 import { generateUrn } from './generateUrn';
 
 export function makeAtom(articles: Article[], feedUid: string, feedTitle: string, feedLink: string): string {
-  const feed = create({ version: '1.0', encoding: 'UTF-8' })
-    .ele('feed', { xmlns: 'http://www.w3.org/2005/Atom' })
-      .ele('id').txt(feedUid).up()
-      .ele('title').txt(feedTitle).up()
-      .ele('link', { href: feedLink });
+ const builder = new XMLBuilder({
+   ignoreAttributes: false,
+   format: true,
+   attributeNamePrefix: "@_"
+ });
 
-  articles.forEach(article => {
-    feed.ele('entry')
-      .ele('title').txt(article.title).up()
-      .ele('link', { href: `/#/newsDetail/${article.columnId}/${article.id}` }).up()
-      .ele('updated').txt(article.publishTime).up()
-      .ele('id').txt(article.link ? generateUrn() : article.id).up()
-      .ele('content', { type: 'html' }).txt(escape(article.content));
-  });
+ const feed = {
+   feed: {
+     "@_xmlns": "http://www.w3.org/2005/Atom",
+     id: feedUid,
+     title: feedTitle,
+     link: { "@_href": feedLink },
+     updated: articles[0].publishTime,
+     entry: articles.map(article => ({
+       title: article.title,
+       link: { "@_href": `/#/newsDetail/${article.columnId}/${article.id}` },
+       updated: article.publishTime,
+       id: article.link ? generateUrn() : article.id,
+       content: {
+         "@_type": "html",
+         "#text": article.content
+       }
+     }))
+   }
+ };
 
-  feed.ele('updated').txt(articles[0].publishTime);
-
-  return feed.end({ prettyPrint: true });
+ return builder.build(feed);
 }
