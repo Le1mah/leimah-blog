ef433ec23fc356957072c92e5e78ab1feb2b028a
Le1mah <leimahhamiel@gmail.com>
2025-05-12 02:02:19 +0800

    docs: 05-12 say something

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,15 @@ import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
+  const articles = [{
+    title: "Review of Review",
+    date: "2024-02-28",
+    slug: "2024/02/review-of-review",
+  }, {
+    title: "Say Something",
+    date: "2025-05-12",
+    slug: "2025/01/say-something",
+  }]
   return (
     <>
       <h1>Welcome to Leimah's Blog</h1>
@@ -10,9 +19,13 @@ export default component$(() => {
         <h2>Blog list:</h2>
 
         <ul>
-          <li>
-            <a href="/blog/2024/02/review-of-review">[2024-02-28] Review of Review</a>
-          </li>
+          {articles.map((article) => (
+            <li key={article.slug}>
+              <a href={`/blog/${article.slug}`}>
+                [{article.date}] {article.title}
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
     </>
